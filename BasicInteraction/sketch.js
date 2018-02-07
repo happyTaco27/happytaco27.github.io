76bdceed979ac0cde23d56a904da097196ebe230
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-02-07 15:20:59 -0600

    everything

---
+++ BasicInteraction/sketch.js
@@ -0,0 +1,12 @@
+function setup()  {
+  createCanvas(windowWidth,windowHeight);
+}
+function draw()   {
+
+}
+
+function mousePressed()  {
+  fill(random(255), random(255), random(255))
+  ellipse(mouseX,mouseY,100,100);
+
+}
