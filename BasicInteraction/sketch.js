ddd8ea8d5db74ddff9d209a783f2f98a501eaab2
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-02-08 14:41:06 -0600

    updated with some of the text stuff

--- BasicInteraction/sketch.js
+++ BasicInteraction/sketch.js
@@ -4,9 +4,13 @@ function setup()  {
 function draw()   {
 
 }
+function deviceShaken() {
+textFont("Times New Roman")
+textSize(32)
+}
 
 function mousePressed()  {
   fill(random(255), random(255), random(255))
   ellipse(mouseX,mouseY,100,100);
-  
+
 }
