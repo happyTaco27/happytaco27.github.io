34386818db97463562ebe9db5402655e0d587e06
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-02-08 15:20:04 -0600

    mouse and keyboard

--- clone/sketch.js
+++ clone/sketch.js
@@ -1,34 +1,34 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
-
-// global variables
-let gear;
-
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  document:addEventListener("contextmenu", event => event.preventDefault())
+
 }
 
 function draw() {
-  background(255);
+  //background(255);
 
-  image(gear, 0, 0);
-
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
-  noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
 }
+
+function mousePressed() {
+  if (mouseButton === LEFT) {
+    noStroke();
+    fill(random(255), random(255), random(255));
+    rect(mouseX, mouseY, 50, 50);
+  }
+  if (mouseButton === RIGHT) {
+    noStroke();
+    fill(random(255), random(255), random(255));
+    ellipse(mouseX, mouseY, 50, 50);
+  }
+
+
+}
+
+function keyPressed() {
+  if (key === "w"|| key === "W")  {
+    background(255);
+    }
+  if (key === "v"|| key === "V")  {
+    background(0);
+    }
+  }
