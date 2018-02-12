f9409731bdeaf7344d1e6e62f096e807321167ad
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-02-12 14:32:44 -0600

    space key now changes square

---
+++ overridekeys/sketch.js
@@ -0,0 +1,31 @@
+let lightSwitch = true;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  let lightSwitch = true;
+
+}
+
+function draw() {
+  drawLight();
+}
+
+function keyTyped() {
+  if (key === ' ') {
+    lightSwitch = !lightSwitch;
+  }
+  else {
+    return false;
+  }
+}
+
+function drawLight() {
+  if (lightSwitch === true) {
+    fill(255);
+  } else {
+    fill(0)
+  }
+  rectMode(CENTER)
+  rect(width / 2, height / 2, 200, 200);
+
+}
