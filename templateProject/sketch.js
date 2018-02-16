7e5104a79ce222707812384010ae415ee601d172
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-02-16 14:37:07 -0600

    fixing your problems

--- templateProject/sketch.js
+++ templateProject/sketch.js
@@ -1,5 +1,21 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 
-function setup(){
-  createCanvas(windowWidth,windowHeight);
-  noStroke()
+}
+
+
+function interactiveBackground() {
+  let a;
+  let b;
+  a = mouseX/6;
+  b = mouseY/6;
+  rect(width/2-a ,height/2-b ,100 ,100 );
+
+
+}
+
+function draw() {
+  background(255);
+  interactiveBackground();
+
 }
