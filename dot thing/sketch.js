36bd5b0e927adfaa9d2a1570b6281ba585fea3e1
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-03-02 15:19:55 -0600

    dots

--- dot thing/sketch.js
+++ dot thing/sketch.js
@@ -1,34 +1,15 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
 
-// global variables
-let gear;
 
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
+function setup(){
+  createCanvas(windowWidth,windowHeight);
 }
 
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function draw(){
+  dots();
 }
-
-function draw() {
-  background(255);
-
-  image(gear, 0, 0);
-
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
-  noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+function dots(){
+  for(let a =20;a<400;a+=20);
+    for(let b=20;ab<800;b+=20);
+    point(a,b)
+  }
 }
