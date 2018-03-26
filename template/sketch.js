a1c77c578f346eb673e4a3117c42c128ec6cb2db
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-03-26 14:25:36 -0600

    yep

--- template/sketch.js
+++ template/sketch.js
@@ -1,36 +1,9 @@
-let ball=[];
-function setup(){
-  createCanvas(windowWidth,windowHeight);
+// Donovan
+// March 26, 2018 - replace with the date
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 }
-function draw(){
+
+function draw() {
   background(255);
-  moveBalls();
-  displayBalls();
-}
-function mouseClicked(){
-  let aBall={
-    x:mouseX,
-    y:mouseY,
-    size:random(10,50),
-    color:color(random(255),(random(255),(random(255)),
-    dx:random(-5,5),
-    dy:random(-5,5),
-  };
-  ball.push(aBall)
-}
-function displayBalls(){
-  for( let i=0;i<balls.length;i++){
-    fill(ball[i].color);
-    ellipse(ball[i].x,ball[i].y,ball[i].size,ball[i].ball[i].size)
-  }
-}
-function moveBalls(){
-  for( let i=0;i<balls.length;i++){
-    ball[i].x,ball[i].dx;
-    ball[i].y,ball[i].dy;
-    if(ball[i].y<0){
-      ball[i].y=height;
-    if
-    }
-  }
 }
