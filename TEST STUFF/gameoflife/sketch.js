1e2bdae470535ddb52486481f506a1738167bd4b
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-04-30 15:17:27 -0600

    player pos is almost done

--- TEST STUFF/gameoflife/sketch.js
+++ TEST STUFF/gameoflife/sketch.js
@@ -5,22 +5,30 @@ let grid;
 let cellSize;
 let xcoord=[];
 let ycoord=[];
+let player;
 function setup() {
   createCanvas(windowWidth,windowHeight);
   cellSize = height / cols;
   //map is inverted
   grid=[
-    [0,0,0,0,0,0],
-    [0,1,1,1,1,0],
-    [0,1,5,1,1,0],
-    [2,3,1,1,1,0],
-    [4,2,3,1,1,0],
-    [4,4,2,0,0,0],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1]
+    // [0,0,0,0,0,0],
+    // [0,1,1,1,1,0],
+    // [0,1,5,1,1,0],
+    // [2,3,1,1,1,0],
+    // [4,2,3,1,1,0],
+    // [4,4,2,0,0,0],
   ];
 }
 function draw() {
   background(255);
   displayGrid();
+
 }
 //Leave this, it's isnt working
 // class Map {
@@ -64,11 +72,11 @@ function displayGrid() {
         fill(255);
       }
       else if (grid[x][y]===5){
-
+        player=true;
         fill(0,255,0);
       }
       else {
-        fill(0);
+        fill(255);
       }
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
@@ -81,45 +89,48 @@ function mousePressed() {
   ycoord = floor(mouseY / cellSize);
   let currentPos=[];
   let priorPos=[];
-  if (mouseIsPressed){
-    // if(grid[xcoord][ycoord]===){
-    //
-    // }
+  if (mouseIsPressed&&grid[xcoord][ycoord]===1&&grid[xcoord][ycoord]!==5){
     print("x:",xcoord," y:",ycoord," and state: ",grid[xcoord][ycoord]);
-
-    // }
-    //checking if neighbour tiles are a player, doesnt work yet(WIP)
-    // for(let i=0;i<2;i++){
-    //   for(let j=0;j<2;j++){
-    //     if(grid[i][j]!==grid[xcoord][ycoord]&&grid[i][j]===5){
-    //       priorPos=grid[i][j];
-    //       grid[i][j]=1;
-    // }
-    // }
-    // }
-  }
-}
-class PlayerCheck {
-  constructor() {
-    this.playPos;
-    this.currentpos=[];
-    this.priorPos;
-    this.xcoord=xcoord;
-    this.ycoord=ycoord;
-  }
-  checkIfPlayerIsOnMap()  {
-    for(let i=0;i<cols;i++){
-      for(let j=0;j<rows;j++){
-        if(grid[i][j]===5){
-          this.currentPos=grid[i][j];
+    grid[xcoord][ycoord]=5;
+    //Prior player pos removal from grid
+    for(let i=0;i<2;i++){
+      for(let j=0;j<2;j++){
+        if(grid[xcoord-1][ycoord-1]===5||
+          grid[xcoord][ycoord-1]===5||
+          grid[xcoord+1][ycoord-1]===5||
+          grid[xcoord-1][ycoord]===5||
+          grid[xcoord+1][ycoord]===5||
+          grid[xcoord-1][ycoord+1]===5||
+          grid[xcoord][ycoord+1]===5||
+          grid[xcoord+1][ycoord+1]===5){
+            
+          grid[i][j]=1;
         }
       }
     }
   }
-  move(){
-    if(this.currentPos!==grid[this.xcoord][this.ycoord]){
-      this.currentPos=this.priorPos;
-
-    }
-  }
 }
+// class PlayerCheck {
+//   constructor() {
+//     this.playPos;
+//     this.currentpos=[];
+//     this.priorPos;
+//     this.xcoord=xcoord;
+//     this.ycoord=ycoord;
+//   }
+//   checkIfPlayerIsOnMap()  {
+//     for(let i=0;i<cols;i++){
+//       for(let j=0;j<rows;j++){
+//         if(grid[i][j]===5){
+//           this.currentPos=grid[i][j];
+//         }
+//       }
+//     }
+//   }
+//   move(){
+//     if(this.currentPos!==grid[this.xcoord][this.ycoord]){
+//       this.currentPos=this.priorPos;
+//
+//     }
+//   }
+// }
