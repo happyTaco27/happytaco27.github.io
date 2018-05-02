c8a15f10415ed6f1dc5673b516460c76125279db
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-05-02 16:56:01 -0600

    Revert "Merge branch 'master' of https://github.com/happyTaco27/happytaco27.github.io"
    This reverts commit c390a0661a541adb26e4771fd2cc29f362df05c9, reversing
    changes made to e5aee9ac7df76273467d8a5cc1d7445eee928343.

--- TEST STUFF/gameoflife/sketch.js
+++ TEST STUFF/gameoflife/sketch.js
@@ -1,12 +1,11 @@
 //this is for testing
+let rows = 6;
+let cols = 6;
 let grid;
-let rows=8;
-let cols=8;
 let cellSize;
 let xcoord=[];
 let ycoord=[];
 let player;
-<<<<<<< HEAD
 let gridState;
 let startPoint;
 let startfloor;
@@ -16,20 +15,18 @@ function preload(){
   startfloor= loadStrings(startPoint);
 }
 
-=======
->>>>>>> a5f042383b0f737b1519daae75e7dec5fff27a15
 function setup(){
   createCanvas(windowWidth,windowHeight);
   cellSize = height / cols;
+  gridState=1;
+  //map is inverted
   grid=[
-    [0,0,0,0,0,0,0,0],
-    [0,1,1,1,1,1,1,0],
-    [0,1,5,1,1,1,1,0],
-    [0,1,1,1,1,1,1,0],
-    [0,1,1,1,1,1,1,0],
-    [0,1,1,1,1,1,1,0],
-    [0,1,1,1,1,1,1,0],
-    [0,0,0,0,0,0,0,0]
+    [0,0,0,0,0,0],
+    [0,1,1,1,1,0],
+    [0,1,5,1,1,0],
+    [0,1,1,1,1,0],
+    [0,1,1,1,1,0],
+    [0,0,0,0,0,0],
   ];
 }
 
@@ -41,7 +38,6 @@ function draw() {
 function displayGrid() {
   for (let x=0; x<cols; x++) {
     for (let y=0; y<rows; y++) {
-<<<<<<< HEAD
       //wall
       if (grid[x][y] === 0) {
         fill(255);
@@ -64,24 +60,6 @@ function displayGrid() {
       }
       //player
       else if (grid[x][y]===5){
-=======
-      if(grid[x][y]===0||grid[x][y]==="0") {
-        fill(255);
-      }
-      else if(grid[x][y]===1||grid[x][y]==="1") {
-        fill(175);
-      }
-      else if(grid[x][y]===2||grid[x][y]==="2") {
-        fill(255);
-      }
-      else if(grid[x][y]===3||grid[x][y]==="3") {
-        fill(255);
-      }
-      else if(grid[x][y]===4||grid[x][y]==="4") {
-        fill(255);
-      }
-      else if(grid[x][y]===5||grid[x][y]==="5"){
->>>>>>> a5f042383b0f737b1519daae75e7dec5fff27a15
         player=true;
         fill(0,255,0);
       }
@@ -92,38 +70,30 @@ function displayGrid() {
     }
   }
 }
+
 function mousePressed() {
 //testing position
   xcoord = floor(mouseX / cellSize);
   ycoord = floor(mouseY / cellSize);
-  if(grid[xcoord][ycoord]===0||grid[xcoord][ycoord]==="0"){
+  let currentPos=[];
+  let priorPos=[];
+  if(grid[xcoord][ycoord]===0){
     grid[xcoord][ycoord];
   }
   else{
     //Prior player pos removal from grid
     for(let i=0;i<rows;i++){
       for(let j=0;j<cols;j++){
-<<<<<<< HEAD
         //checking if neighbour tiles are walls (leaving it as the same)
         if(grid[i][j]===0||grid[i][j]===2||grid[i][j]===3||grid[i][j]===4){
           grid[i][j];
         }
         //checking if a neighbour tile is a player (changing it to a floor/empty tile)
         else if(grid[i][j]===5){
-=======
-        if(grid[i][j]===0||grid[i][j]==="0"||
-          grid[i][j]===2||grid[i][j]==="2"||
-          grid[i][j]===3||grid[i][j]==="3"||
-          grid[i][j]===4||grid[i][j]==="4"){
-          grid[i][j];
-        }
-        //reseting players prior pos
-        else if(grid[i][j]===5||grid[i][j]==="5"){
->>>>>>> a5f042383b0f737b1519daae75e7dec5fff27a15
           grid[i][j]=1;
         }
         //changing players pos
-        else if(mouseIsPressed&&grid[xcoord][ycoord]===1||grid[xcoord][ycoord]==="1"){
+        else if(mouseIsPressed&&grid[xcoord][ycoord]===1){
           print("x:",xcoord+1," y:",ycoord+1," and state: ",grid[xcoord][ycoord]);
           grid[xcoord][ycoord]=5;
         }
