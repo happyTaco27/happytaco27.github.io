1d2c7d22ba2cae6fda780cc5ed690838bf0bd71c
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-05-14 15:14:38 -0600

    fixing broken stuff

--- TEST STUFF/rouge game/sketch.js
+++ TEST STUFF/rouge game/sketch.js
@@ -5,24 +5,19 @@ let cols=8;
 let cellSize;
 let xcoord=[];
 let ycoord=[];
-let w,q;
-function preload(){
-  q="Assets/Floor_1.txt";
-}
+let dX,dY;
 function setup(){
   createCanvas(windowWidth,windowHeight);
   cellSize = height / cols;
-  //inverts map and Flips the map 90* to the right
-  // grid=createEmpty2dArray(rows,cols);
   grid=[
-    [0,0,0,0,0,0,0],
-    [0,1,1,1,1,1,0],
-    [0,1,5,1,1,1,0],
-    [0,1,1,1,1,1,0],
-    [0,1,1,1,1,1,0],
-    [0,0,0,0,0,1,0],
-    [0,1,1,1,1,1,0],
-    [0,0,0,0,0,0,0]
+    [2,2,2,2,2,2,2,2],
+    [2,1,1,1,1,1,1,2],
+    [2,1,5,1,1,1,1,2],
+    [2,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,2],
+    [2,2,2,2,2,2,2,2]
   ];
 }
 function draw() {
@@ -30,49 +25,31 @@ function draw() {
   displayGrid();
 }
 function displayGrid() {
-  for (let x=0; x<cols; x++) {
-    for (let y=0; y<rows; y++) {
-      let tileType=grid[x][y];
-      grid[x][y]=tileType;
-      blockType(grid[x][y],x,y);
+  for(let x=0; x<cols; x++) {
+    for(let y=0; y<rows; y++) {
+      if(grid[x][y]===2) {
+        fill(255);
+      }
+      else if(grid[x][y]===1) {
+        fill(175);
+      }
+      else if(grid[x][y]===5){
+        fill(0,255,0);
+      }
+      else {
+        fill(255);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
-  }
-}
-function blockType(type,x,y){
-  if(type===0||type==="0") {
-    fill(255);
-    rect(x,y,cellSize,cellSize);
-  }
-  else if(type===1||type==="1") {
-    fill(175);
-    rect(x,y,cellSize,cellSize);
-  }
-  else if(type===2||type==="2") {
-    fill(255);
-    rect(x,y,cellSize,cellSize);
-  }
-  else if(type===3||type==="3") {
-    fill(255);
-    rect(x,y,cellSize,cellSize);
-  }
-  else if(type===4||type==="4") {
-    fill(255);
-    rect(x,y,cellSize,cellSize);
-  }
-  else if(type===5||type==="5"){
-    fill(0,255,0);
-    rect(x,y,cellSize,cellSize);
-  }
-  else {
-    fill(255);
   }
 }
 function mousePressed() {
 //testing position
   xcoord = floor(mouseX / cellSize);
   ycoord = floor(mouseY / cellSize);
-  if(grid[xcoord][ycoord]===0||grid[xcoord][ycoord]==="0"){
+  if(grid[xcoord][ycoord]===2){
     grid[xcoord][ycoord];
+    print("that's a wall!");
   }
   else{
     //Prior player pos removal from grid
@@ -86,24 +63,14 @@ function mousePressed() {
         }
         //reseting players prior pos
         else if(grid[i][j]===5||grid[i][j]==="5"){
-          grid[i][j]="1";
+          grid[i][j]=1;
         }
         //changing players pos
         else if(mouseIsPressed&&grid[xcoord][ycoord]===1||grid[xcoord][ycoord]==="1"){
           print("x:",xcoord+1," y:",ycoord+1," and state: ",grid[xcoord][ycoord]);
-          grid[xcoord][ycoord]="5";
+          grid[xcoord][ycoord]=5;
         }
       }
     }
   }
-}
-function createEmpty2dArray(cols, rows) {
-  let randomGrid = [];
-  for (let x = 0; x < cols; x++) {
-    randomGrid.push([]);
-    for (let y = 0; y < rows; y++) {
-      randomGrid[x].push(0);
-    }
-  }
-  return randomGrid;
 }
